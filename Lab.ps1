10f850226f04a9011544748bc41a700ad5ef8e6a
mohssine <mohssine-d@hotmail.com>
2019-12-28 19:38:48 +0100

    ADFS Lab -1

--- Lab.ps1
+++ Lab.ps1
@@ -1,3 +1,50 @@
+#region Adfs Lab
+
+Install-Script -Name Request-Certificate
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install googlechrome
+
+
+
+$domainName = "{{ windows_domain_info['dns_domain_name'] }}"
+$password = "{{ windows_domain_info['domain_admin_password'] }}"
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$fqdn = [System.Net.Dns]::GetHostByName(($env:computerName)) | FL HostName | Out-String | %{ "{0}" -f $_.Split(':')[1].Trim() };
+$filename = "C:\$fdqn.pfx"
+$user = "{{ windows_domain_info['dns_domain_name'] }}\{{ windows_domain_info['domain_admin_user'] }}"
+$credential = New-Object `
+    -TypeName System.Management.Automation.PSCredential `
+    -ArgumentList $user, $securePassword
+
+Write-Host "Installing nuget package provider"
+Install-PackageProvider nuget -force
+
+Write-Host "Installing PSPKI module"
+Install-Module -Name PSPKI -Force
+
+Write-Host "Importing PSPKI into current environment"
+Import-Module -Name PSPKI
+
+Write-Host "Generating Certificate"
+$selfSignedCert = New-SelfSignedCertificateEx `
+    -Subject "CN=$fqdn" `
+    -ProviderName "Microsoft Enhanced RSA and AES Cryptographic Provider" `
+    -KeyLength 2048 -FriendlyName 'OAFED SelfSigned' -SignatureAlgorithm sha256 `
+    -EKU "Server Authentication", "Client authentication" `
+    -KeyUsage "KeyEncipherment, DigitalSignature" `
+    -Exportable -StoreLocation "LocalMachine"
+$certThumbprint = $selfSignedCert.Thumbprint
+
+Write-Host "Installing ADFS"
+Install-WindowsFeature -IncludeManagementTools -Name ADFS-Federation
+
+Write-Host "Configuring ADFS"
+Import-Module ADFS
+Install-AdfsFarm -CertificateThumbprint $certThumbprint -FederationServiceName $fqdn -ServiceAccountCredential $credential
+
+
+#endregion
+
 #sdn02
 $DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
 Start-BitsTransfer -Source 'https://raw.githubusercontent.com/drdmohssine/MSRepo/master/Autounattend.xml' -Destination "$DesktopPath\Autounattend.xml"
